800e9faefee116899ed7ea4d1e3c6ad564fdb205
tothand <toti@Fosszas-Mac-mini.local>
2016-05-10 00:04:28 +0200

    Initial commit

---
+++ parse.py
@@ -0,0 +1,56 @@
+#!/usr/local/bin/python
+import ConfigParser
+
+bannedsections =[
+'HKEY_CURRENT_USER\Software\SimonTatham',
+'HKEY_CURRENT_USER\Software\SimonTatham\PuTTY',
+'HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Jumplist',
+'HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions',
+'HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\SshHostKeys']
+
+def NumberConvert(dwordStr):
+    if dwordStr.startswith('dword'):
+        hexstr = dwordStr.replace('dword:','')
+        num = int(hexstr,16)
+        return num
+    return dwordStr
+
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            #print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+
+Config = ConfigParser.SafeConfigParser()
+Config.read("./putty-utf8.reg")
+
+for sec in Config.sections():
+#    print sec
+#    print bannedsections
+#    exit(1)
+    if sec in bannedsections:
+        continue
+    values = ConfigSectionMap(sec)
+    try:
+        if values['"protocol"'] == '"ssh"':
+            print "Host %s" % sec[sec.rfind('\\')+1:]
+            print "\tHostName %s" % values['"hostname"'].strip('"')
+            if values['"username"']:
+                print "\tUser %s" % values['"username"'].strip('"')
+            if values['"portnumber"'] and values['"portnumber"'] != "dword:00000016":
+                print "\tPort %s" % NumberConvert(values['"portnumber"'].strip('"'))
+            if values['"publickeyfile"']:
+                ifile = values['"publickeyfile"'].strip('"')
+                print "\tIdentityFile ~/.ssh/%s.pem" % ifile[ifile.rfind('\\')+1:ifile.rfind('.')]
+            print ""
+    except:
+        continue
+#    exit(1)
